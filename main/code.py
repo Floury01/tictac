179dd38f6c987176573be400dabf397cbb10ce89
Floury01 <e.akimov07@yandex.ru>
2025-10-20 00:33:03 +0300

    Функция игры

--- main/code.py
+++ main/code.py
@@ -13,3 +13,53 @@ def checkwin(board):
 
 def is_board_full(board):
     return " " not in board
+
+def play_game():
+    board = [" " for _ in range(9)]
+    current_player = "X"
+    
+    print("Добро пожаловать в Крестики-нолики!")
+    print("Для хода введите число от 1 до 9:")
+    print(" 1 | 2 | 3 ")
+    print("-----------")
+    print(" 4 | 5 | 6 ")
+    print("-----------")
+    print(" 7 | 8 | 9 ")
+    print()
+    
+    while True:
+        field(board)
+        print(f"\nХод игрока {current_player}")
+        
+        try:
+            move = int(input("Введите номер клетки (1-9): ")) - 1
+            if move < 0 or move > 8:
+                print("Пожалуйста, введите число от 1 до 9!")
+                continue
+                
+            if board[move] != " ":
+                print("Эта клетка уже занята!")
+                continue
+                
+        except ValueError:
+            print("Пожалуйста, введите число!")
+            continue
+        
+        board[move] = current_player
+        
+        winner = checkwin(board)
+        if winner:
+            field(board)
+            print(f"\nПобедил игрок {winner}!")
+            break
+            
+        if is_board_full(board):
+            field(board)
+            print("\nНичья!")
+            break
+        
+        current_player = "O" if current_player == "X" else "X"
+
+if __name__ == "__main__":
+    play_game()
+    
